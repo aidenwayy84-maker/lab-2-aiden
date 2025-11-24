6706010b4bb32584caa05d8c730ee98a8b3d24db
aidenwayy84-maker <aidenwayy84@gmail.com>
2025-11-24 22:33:54 +0000

    lab2

---
+++ calculations/second.js
@@ -0,0 +1,8 @@
+// Lab assignment 2.5 - second javascript file 
+// Calculate body mass undex (BMI)
+const weightkg =70;  // example weghit in kilograms
+const heightM = 1.75;  // example height in meters
+const bmi = weightkg / (heightM * heightM);
+console.log("weight (kg):", weightkg);
+console.log("height (m):", heightM);
+console.log("BMI =" , bmi); 
---
+++ index.js
@@ -0,0 +1,18 @@
+// lab assignment 2.4 main     - javascript file
+
+// 1. simple addition
+const a=15;
+const b=20;
+const sum = a+b;
+console.log ("1) sum of a+b=", sum);
+
+// 2. area of a rectangle 
+const width = 4;
+const height = 3;
+const area = width * height; 
+console.log ("2) area of rectangle=", area);
+
+// 3. temperature conversion celsius to fahrenheit 
+const celsius = 25;
+const fahrenheit = celsius *9/5+32;
+console.log("3) temperature in fahrenheit =", fahrenheit);
